25f022839648eec4053f0d4bd200e6729648d8c0
gm <gunter.miegel@rgsqd.de>
2024-10-20 08:26:53 +0200

    Simplified code

--- python/main.py
+++ python/main.py
@@ -1,16 +1,20 @@
 import json
 import os
 import time
+import webbrowser
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+
 class ResumeHandler(FileSystemEventHandler):
-    def __init__(self, json_path, html_template_path):
-        self.json_path = json_path
-        self.html_template_path = html_template_path
-        self.env = Environment(loader=FileSystemLoader(os.path.dirname(html_template_path)),
-                               autoescape=select_autoescape(['html', 'xml']))
+    def __init__(self):
+        self.json_path = os.path.abspath("input/resume.json")
+        # Jinja template Environment
+        self.env = Environment(
+            loader=FileSystemLoader(os.path.dirname("input/templates/template.html")),
+            autoescape=select_autoescape(["html", "xml"]),
+        )
 
         # Render output on startup
         print("Render on startup")
@@ -18,82 +22,65 @@ class ResumeHandler(FileSystemEventHandler):
         self.render_template()
 
     def on_modified(self, event):
-        if event.src_path == self.json_path:
-            print(f"Detected changes in {self.json_path}, updating data...")
-            self.load_data()
-            self.render_template()
-        elif event.src_path == self.html_template_path:
-            print(f"Detected changes in {self.html_template_path}, reloading template...")
-
+        print(event)
+        print(f"Detected changes in {event.src_path}")
+        self.load_data()
+        self.render_template()
 
     def load_data(self):
         try:
             # Load the updated resume data from the JSON file
-            with open(self.json_path) as f:
-                self.resume_data = json.load(f)
+            with open(self.json_path) as json_file:
+                self.resume_data = json.load(json_file)
             print("JSON resume data")
-        except Exception as e:
-            print(f"Error loading data from {self.json_path}: {e}")
-
-
-    def update_data(self):
-        try:
-            # Load the updated resume data from the JSON file
-            resume_data = self.resume_data
-            print(resume_data)
-
-            # Render the HTML template with the resume data
-            self.render_template()
+            print(self.resume_data)
         except Exception as e:
             print(f"Error loading data from {self.json_path}: {e}")
 
     def render_template(self):
         try:
             # Load the template
-            template_name = os.path.basename(self.html_template_path)
+            template_name = os.path.basename("templates/template.html")
             template = self.env.get_template(template_name)
+            output_html_path = os.path.abspath("output/output_resume.html")
 
             # Render the template with the resume data
             rendered_html = template.render(self.resume_data)
             # Write the rendered HTML to the output file
-            with open(output_html_path, 'w') as output_file:
+            with open(output_html_path, "w") as output_file:
                 output_file.write(rendered_html)
 
             print(f"Rendered HTML written to {output_html_path}")
             print(f"You can view your rendered resume at: file://{output_html_path}")
+            webbrowser.open(output_html_path, new=2)
         except Exception as e:
             print(f"Error rendering template: {e}")
             exit(1)
 
+
 class ResumeWatcher:
-    def __init__(self, json_path, html_template_path):
-        self.json_path = json_path
-        self.html_template_path = html_template_path
-        self.output_html_path = output_html_path
-        self.event_handler = ResumeHandler(json_path, html_template_path)
+    def __init__(self):
+        self.event_handler = ResumeHandler()
         self.observer = Observer()
 
     def start(self):
-        self.observer.schedule(self.event_handler, path=os.path.dirname(self.json_path), recursive=False)
-        self.observer.schedule(self.event_handler, path=os.path.dirname(self.html_template_path), recursive=False)
+        self.observer.schedule(self.event_handler, path="./input", recursive=True)
         self.observer.start()
-        print(f"Watching for changes in {self.json_path} and {self.html_template_path}...")
+        print(f"Watching for changes in ...")
         try:
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
             self.stop()
 
+        self.observer.join()
+
     def stop(self):
         self.observer.stop()
-        self.observer.join()
         print("Stopped watching.")
 
+
 if __name__ == "__main__":
-    json_path = os.path.abspath('resume.json')
-    html_template_path = os.path.abspath('templates/template.html')
-    output_html_path = os.path.abspath('output_resume.html')
+    watcher = ResumeWatcher()
 
-    watcher = ResumeWatcher(json_path, html_template_path)
     watcher.start()
-
