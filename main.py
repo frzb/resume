01783003524b997e8ad0d932c329add4990537dc
gm <gunter.miegel@rgsqd.de>
2025-01-12 22:30:43 +0100

    Added first draft for German version

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from weasyprint import HTML
 class ResumeHandler(FileSystemEventHandler):
     def __init__(self, include_private_data):
         self.include_private_data = include_private_data
-        self.json_path = os.path.abspath("input/resume.json")
         self.json_path_private = os.path.abspath("input/private/private_resume.json")
+       
         # Jinja template Environment
         self.env = Environment(
             loader=FileSystemLoader(os.path.dirname("input/templates/template.j2")),
@@ -22,21 +22,26 @@ class ResumeHandler(FileSystemEventHandler):
             lstrip_blocks=True,
             autoescape=select_autoescape(["html", "xml"]),
         )
-
         # Render output on startup
         print("Render on startup")
-        self.load_data(include_private_data)
-        self.render_template()
-        self.tailwindcss_build()
+        self.create_output()
+
+    def create_output(self):
+            for file in os.listdir("./input"):
+                print(file)
+                if file.endswith(".json"):
+                    name = f"_{os.path.splitext(os.path.basename(file))[0]}"
+                    data = self.load_data(f"./input/{file}", self.include_private_data)
+                    self.render_template(data, file)
+                    self.tailwindcss_build()
+                    #HTML(f"./output/index{name}.html").write_pdf(f"./output/{name}.pdf")
 
     def on_closed(self, event):
         print(event)
-        if event.src_path in ["./input/resume.json", "./input/templates/template.j2"]:
+        #if event.src_path in ["./input/resume.json", "./input/templates/template.j2"]:
+        if event.src_path in ["./input"]:
             print(f"Detected relevant changes in {event.src_path}")
-            self.load_data(self.include_private_data)
-            self.render_template()
-            self.tailwindcss_build()
-            HTML("./output/index.html").write_pdf("./output/resume.pdf")
+            self.create_output()
 
 
     def merge_dicts(self, dict1, dict2):
@@ -47,21 +52,24 @@ class ResumeHandler(FileSystemEventHandler):
                 dict1[key] = value
         return dict1
 
-    def load_data(self, include_private_data=False):
+    def load_data(self, file, include_private_data=False):
         try:
             # Load the updated resume data from the JSON file
-            with open(self.json_path) as json_file:
-                self.resume_data = json.load(json_file)
+            print(file)
+            with open(file) as json_file:
+                resume_data = json.load(json_file)
             if include_private_data:
                 print("Including private data")
                 with open(self.json_path_private) as json_file:
                     self.resume_data_private = json.load(json_file)
-                    merged = self.merge_dicts(self.resume_data, self.resume_data_private)
-                    self.resume_data = merged
+                    merged = self.merge_dicts(file, self.resume_data_private)
+                    resume_data = merged
             print("JSON resume data")
-            print(self.resume_data)
+            print(resume_data)
+            return(resume_data)
         except Exception as e:
-            print(f"Error loading data from {self.json_path}: {e}")
+            print(f"Error loading data from {file}: {e}")
+            exit(1)
 
     def tailwindcss_build(self):
         # We needed to minify the Tailwiwind CSS file because
@@ -78,18 +86,26 @@ class ResumeHandler(FileSystemEventHandler):
             print("Error occurred while running Tailwind CSS:")
             print(e.stderr)
 
-    def render_template(self):
+    def render_template(self, data, file):
         try:
             # Load the template
             template_name = os.path.basename("templates/template.j2")
             template = self.env.get_template(template_name)
             output_html_path = os.path.abspath("./index.html")
+            output_html_path_de = os.path.abspath("./index_de.html")
 
-            # Render the template with the resume data
-            rendered_html = template.render(self.resume_data)
-            # Write the rendered HTML to the output file
-            with open(output_html_path, "w") as output_file:
-                output_file.write(rendered_html)
+            if not "de" in file:
+                # Render the template with the resume data
+                rendered_html = template.render(data)
+                # Write the rendered HTML to the output file
+                with open(output_html_path, "w") as output_file:
+                    output_file.write(rendered_html)
+            else:
+                # Render the template with the resume data
+                rendered_html = template.render(data)
+                # Write the rendered HTML to the output file
+                with open(output_html_path_de, "w") as output_file:
+                    output_file.write(rendered_html)
 
             print(f"Rendered HTML written to {output_html_path}")
             print(f"You can view your rendered resume at: file://{output_html_path}")
